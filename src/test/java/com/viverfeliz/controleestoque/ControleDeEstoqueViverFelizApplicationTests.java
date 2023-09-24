f76b39e6f80fda762d2585be7f4b49097a2e8151
LucasLima004 <lucaslimasl004@gmail.com>
2023-09-23 21:57:26 -0300

    feat: Initial commit.

---
+++ src/test/java/com/viverfeliz/controleestoque/ControleDeEstoqueViverFelizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viverfeliz.controleestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeEstoqueViverFelizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
