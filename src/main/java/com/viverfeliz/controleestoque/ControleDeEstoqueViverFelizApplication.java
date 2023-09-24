41582336bc064432584396ff72468d0faa5ae379
LucasLima004 <lucaslimasl004@gmail.com>
2023-09-23 21:57:08 -0300

    feat: Initial commit.

---
+++ src/main/java/com/viverfeliz/controleestoque/ControleDeEstoqueViverFelizApplication.java
@@ -0,0 +1,13 @@
+package com.viverfeliz.controleestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeEstoqueViverFelizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeEstoqueViverFelizApplication.class, args);
+	}
+
+}
